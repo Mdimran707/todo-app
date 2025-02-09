d41a1310b0391cd50edc972c6c2a14578efeb5d2
Mdimran707 <112844636+Mdimran707@users.noreply.github.com>
2025-02-09 15:40:00 +0530

    Update api.js
    added backend url from render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/todos";
+const API_URL = "https://todo-app-backend-97pc.onrender.com";
 
 export const getTodos = async () => await axios.get(API_URL);
 export const addTodo = async (text) => await axios.post(API_URL, { text });
